fed8c8f9192e76ca1727c6983cfc78e5ae9f1418
armanbance <armanbance@gmail.com>
2024-10-03 20:49:16 -0700

    added mongosessionstorage

---
+++ src/mongoSessionStorage.ts
@@ -0,0 +1,24 @@
+// SQLiteSessionStorage.ts
+// /src/db/mongodbSessionStorage.ts
+// import {shopifyApp} from '@shopify/shopify-app-express';
+// import {MongoDBSessionStorage} from '@shopify/shopify-app-session-storage-mongodb';
+
+// const shopify = shopifyApp({
+//   sessionStorage: new MongoDBSessionStorage(
+//     'mongodb://username:password@host/',
+//     'database',
+//   ),
+//   // ...
+// });
+
+// OR
+
+// const shopify = shopifyApp({
+//   sessionStorage: MongoDBSessionStorage.withCredentials(
+//     'host.com',
+//     'thedatabase',
+//     'username',
+//     'password',
+//   ),
+//   // ...
+// });
